8d753423b27811ad2aec6fab6420411b2b41621b
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-05-21 11:26:03 -0300

    refactor: removendo codigo morto

--- user/create-user/src/infra/Queue/QueueController.ts
+++ user/create-user/src/infra/Queue/QueueController.ts
@@ -1,22 +1,14 @@
-import { IUserService } from "../../application/IUserService";
+import { ICreateUser } from "../../application/ICreateUser";
 import Queue from "./Queue";
 
 export default class QueueController {
 	
 	constructor (
         readonly queue: Queue,
-        readonly userService: IUserService
+        readonly createUser: ICreateUser
     ) {
         queue.consume("user.created.queue", async (input: any) => {
-            await userService.createUser(input);
-            console.log('=============================>>>>>>>>>>>>>>>> PAYLOAD', input);
-            // const dto = new ProcessPaymentDto(input.cardId, input.orderId, input.total);
-			// await processPayment.execute(dto);
-		});
-
-		queue.consume("processPaymentV1", async (input: any) => {
-            console.log('=========================>>>>>> RESPOSTA', input);
-            
+            await createUser.execute(input);
 		});
 	}
 }
