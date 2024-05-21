fc2ade793d11740a25b2cbd6053c06a88fc0de9e
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-05-21 11:18:43 -0300

    fix: resolvendo problema de thread concorrentes ao obter retorno do metodo

--- ExpenseMaster/user/src/main/java/com/expensemaster/user/api/UserController.java
+++ ExpenseMaster/user/src/main/java/com/expensemaster/user/api/UserController.java
@@ -1,13 +1,17 @@
 package com.expensemaster.user.api;
 
 import com.expensemaster.application.user.IUserService;
+import com.expensemaster.application.user.UserCreatedDto;
 import com.expensemaster.application.user.dto.CreateUserDto;
 import com.expensemaster.user.IUserSpan;
 import com.expensemaster.user.api.input.CreateUserInput;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 @RestController
 public class UserController implements IUserAPI {
@@ -24,15 +28,14 @@ public class UserController implements IUserAPI {
     }
 
     @Override
-    public void createUser(final HttpServletRequest request, final CreateUserInput input) {
+    public ResponseEntity<UserCreatedDto> createUser(final HttpServletRequest request, final CreateUserInput input) {
         this.userSpan.setHttpRequest(request);
-        final var dto = new CreateUserDto(
-                input.name(),
-                input.email(),
-                input.password(),
-                input.birthdate(),
-                input.totalLimit(),
-                input.startDate());
-        this.userSpan.startSpan("userController.create.user", () -> userService.createUser(dto));
+        final var dto = new CreateUserDto(input.email(), input.password(), input.userType());
+        AtomicReference<UserCreatedDto> output = new AtomicReference<UserCreatedDto>();
+        this.userSpan.startSpan("userController.create.user", () -> {
+           output.set(userService.createUser(dto));
+        });
+
+        return new ResponseEntity<>(output.get(), HttpStatus.OK);
     }
 }
