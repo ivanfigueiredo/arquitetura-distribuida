4e5a915129217a9f744a3d222c6def0d39838a82
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-05-21 11:18:06 -0300

    refactor: removendo campos sem utilidade

--- ExpenseMaster/user/src/main/java/com/expensemaster/user/api/input/CreateUserInput.java
+++ ExpenseMaster/user/src/main/java/com/expensemaster/user/api/input/CreateUserInput.java
@@ -3,10 +3,7 @@ package com.expensemaster.user.api.input;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record CreateUserInput(
-        @JsonProperty("name") String name,
         @JsonProperty("email") String email,
         @JsonProperty("password") String password,
-        @JsonProperty("birthdate") String birthdate,
-        @JsonProperty("totalLimit") int totalLimit,
-        @JsonProperty("startDate") String startDate
+        @JsonProperty("userType") String userType
 ) {}
