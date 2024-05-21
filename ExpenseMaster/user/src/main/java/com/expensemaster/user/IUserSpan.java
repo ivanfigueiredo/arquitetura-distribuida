0c3cabc7803b68793cba3c0b65f51b77628f6bf2
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-05-21 11:19:33 -0300

    feat: contrato de interface para propagacao de contexto aos headers na requisicao HTTP

--- ExpenseMaster/user/src/main/java/com/expensemaster/user/IUserSpan.java
+++ ExpenseMaster/user/src/main/java/com/expensemaster/user/IUserSpan.java
@@ -3,9 +3,16 @@ package com.expensemaster.user;
 import com.expensemaster.application.ICallback;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.amqp.core.MessageProperties;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+
+import java.util.List;
 
 public interface IUserSpan {
     public void setHttpRequest(final HttpServletRequest request);
+
     public void startSpan(final String spanName, ICallback function);
-    public MessageProperties contextPropagation();
+
+    public MessageProperties contextPropagationQueue();
+
+    public List<ClientHttpRequestInterceptor> contextPropagationApi();
 }
