f87e80dbbcd6b7bb1d85ff7df54067058f3c9493
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-05-22 21:21:34 -0300

    feat: resolvendo dependencia no main layer

--- user/auth/src/main.ts
+++ user/auth/src/main.ts
@@ -1,18 +1,20 @@
-import { UserService } from './application/UserService';
-import { AuthDatabase } from './infra/AuthDatabase';
+import { Auth } from './application/Auth';
 import { ExpressAdapter } from './infra/ExpressAdapter';
-import { MainCoontroller } from './infra/MainController';
-import { MongoDBAdapter } from './infra/MongoDBAdapter';
-import { TraceContext } from './infra/TraceContext';
+import { MainController } from './infra/MainController';
+import { OpenTelemetrySDK } from './infra/OpenTelemetrySDK';
+import { PostgresAdapter } from './infra/PostgresAdapter';
+import { SpanAdapter } from './infra/SpanAdapter';
+import { UserDatabase } from './infra/UserDatabase';
 
 async function init() {
-	
-    const traceContext = new TraceContext();
-    const expressAdapter = new ExpressAdapter(traceContext);
-    const database = new MongoDBAdapter();
-    const userDAO = new AuthDatabase(database, traceContext);
-    const service = new UserService(userDAO);
-    new MainCoontroller(expressAdapter, service);
+    new OpenTelemetrySDK();
+    const spanAdapter = new SpanAdapter();
+    const expressAdapter = new ExpressAdapter(spanAdapter);
+    const database = new PostgresAdapter();
+    await database.init();
+    const repository = new UserDatabase(database);
+    const usecase = new Auth(repository);
+    new MainController(expressAdapter, usecase);
 
     expressAdapter.listen(6000, () => {console.log('Rodando na porta 6000')});
 }
