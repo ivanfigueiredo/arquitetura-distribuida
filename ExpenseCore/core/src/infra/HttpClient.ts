1143038f3523f5a4f645494e854558f9b1033d37
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-06-01 18:04:27 -0300

    refactor: removendo codigo sem uso

--- ExpenseCore/core/src/infra/HttpClient.ts
+++
@@ -1,4 +0,0 @@
-export interface HttpClient {
-    on: (method: string, url: string, callback: Function) => void;
-    listen: (port: number, callback: Function) => void;
-}
--- ExpenseCore/core/src/infra/MainController.ts
+++
@@ -1,9 +0,0 @@
-import { HttpClient } from "./HttpClient"
-
-export class MainCoontroller {
-    constructor(readonly httpClient: HttpClient) {
-        httpClient.on("post", "/create-user", async function (params: any, data: any) {
-            return data;
-        });
-    }
-}
