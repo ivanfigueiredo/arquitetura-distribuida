f3a20bc500bf7c29b228b6901d3e902e2bcc26ca
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-06-08 14:25:40 -0300

    chore: criando tabela verification_code

--- volumes/flyway/user_db/V1__user.sql
+++ volumes/flyway/user_db/V1__user.sql
@@ -6,11 +6,19 @@ CREATE TABLE "user_type" (
 CREATE TABLE "user" (
     id VARCHAR(36) NOT NULL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     email_verified BOOLEAN DEFAULT FALSE,
     user_type VARCHAR(15) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
 
     CONSTRAINT fk_user_type FOREIGN KEY (user_type) REFERENCES user_type (type)
+);
+
+CREATE TABLE "verification_code" (
+    code_id VARCHAR(36) NOT NULL PRIMARY KEY,
+    email_verify VARCHAR(255) NOT NULL UNIQUE,
+    code VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    expiration_time TIMESTAMP
 );
