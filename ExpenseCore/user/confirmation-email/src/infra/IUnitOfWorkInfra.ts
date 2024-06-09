c38a75c1da9ca48000f6f4c54da4b6aabeb6059c
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-06-08 22:47:52 -0300

    refactor: removendo codigo sem uso

--- ExpenseCore/user/confirmation-email/src/infra/IUnitOfWorkInfra.ts
+++ ExpenseCore/user/confirmation-email/src/infra/IUnitOfWorkInfra.ts
@@ -1,6 +1,4 @@
-import { EntityTarget } from "typeorm";
 import { UserEntity } from "./entities/UserEntity";
-import { VerificationCodeEntity } from "./entities/VerificationCodeEntity";
 
 export interface IUnitOfWorkInfra {
   transaction<T>(data: T): Promise<void>;
