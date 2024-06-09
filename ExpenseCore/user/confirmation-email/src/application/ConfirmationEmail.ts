59f26cafd8a6c780c1d1045a2e561e34d67c1c54
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-06-08 22:48:42 -0300

    refactor: envolvendo usecase com unitofwork para gerenciar transaction e commit no base de dados

--- ExpenseCore/user/confirmation-email/src/application/ConfirmationEmail.ts
+++ ExpenseCore/user/confirmation-email/src/application/ConfirmationEmail.ts
@@ -1,17 +1,29 @@
+import { InternalServerErrorException } from "../infra/exceptions/InternalServerErrorException";
 import { IConfirmationEmail } from "./IConfirmationEmail";
+import { IUnitOfWorkApplication } from "./IUnitOfWorkApplication";
 import { IUserRepository } from "./IUserRepository";
 import { ConfirmationEmailDto } from "./dto/ConfirmationEmailDto";
 
 export class ConfirmationEmail implements IConfirmationEmail {
     constructor(
-        private readonly userRepository: IUserRepository
+        private readonly userRepository: IUserRepository,
+        private readonly unitOfWork: IUnitOfWorkApplication
     ) { }
 
     async execute(dto: ConfirmationEmailDto): Promise<void> {
-        const user = await this.userRepository.finUserByEmailAndCode(dto.email, dto.code);
-        if (user) {
-            user.confirmedEmail();
-            await this.userRepository.save(user)
+        try {
+            await this.unitOfWork.startTransaction();
+            const user = await this.userRepository.finUserByEmailAndCode(dto.email, dto.code);
+            if (user) {
+                user.confirmedEmail();
+                await this.userRepository.save(user)
+            }
+            await this.unitOfWork.commit()
+        } catch (error) {
+            if (error instanceof InternalServerErrorException) {
+                await this.unitOfWork.rollBack()
+            }
+            throw error;
         }
     }
 }
