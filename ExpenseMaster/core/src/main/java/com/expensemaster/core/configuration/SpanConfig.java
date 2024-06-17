10eb6a74c3aaa8a8e322ee8bb2435238f090ffec
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-06-16 22:08:35 -0300

    feat: resolvendo dependencia

--- ExpenseMaster/core/src/main/java/com/expensemaster/core/configuration/SpanConfig.java
+++ ExpenseMaster/core/src/main/java/com/expensemaster/core/configuration/SpanConfig.java
@@ -4,6 +4,7 @@ import com.expensemaster.application.IApplicationSpan;
 import com.expensemaster.core.ISpanAdapter;
 import com.expensemaster.core.SpanAdapter;
 import com.expensemaster.user.IUserSpan;
+import io.opentelemetry.api.logs.LogRecordBuilder;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.propagation.TextMapPropagator;
 import org.springframework.context.annotation.Bean;
@@ -15,17 +16,20 @@ import java.util.Objects;
 public class SpanConfig {
     private final Tracer tracer;
     private final TextMapPropagator textMapPropagator;
+    private final LogRecordBuilder logRecordBuilder;
 
     public SpanConfig(
             final Tracer tracer,
-            final TextMapPropagator textMapPropagator
+            final TextMapPropagator textMapPropagator,
+            final LogRecordBuilder logRecordBuilder
     ) {
         this.textMapPropagator = Objects.requireNonNull(textMapPropagator);
         this.tracer = Objects.requireNonNull(tracer);
+        this.logRecordBuilder = Objects.requireNonNull(logRecordBuilder);
     }
 
     @Bean
     public ISpanAdapter createSpanAdapter() {
-        return new SpanAdapter(tracer, textMapPropagator);
+        return new SpanAdapter(tracer, textMapPropagator, logRecordBuilder);
     }
 }
