0a1e74b62086ed22f51f329eaf62b90e095ca4b5
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-06-27 15:26:12 -0300

    feat: propagacao de contexto

--- ExpenseMaster/core/src/main/java/com/expensemaster/core/SpanAdapter.java
+++ ExpenseMaster/core/src/main/java/com/expensemaster/core/SpanAdapter.java
@@ -1,17 +1,15 @@
 package com.expensemaster.core;
 
 import com.expensemaster.application.ICallback;
-import com.expensemaster.application.IApplicationSpan;
 import com.expensemaster.user.IUserSpan;
 import io.opentelemetry.api.logs.LogRecordBuilder;
+import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
 import io.opentelemetry.context.propagation.TextMapPropagator;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.stereotype.Component;
@@ -22,13 +20,13 @@ import java.util.List;
 import java.util.Objects;
 
 @Component
-public class SpanAdapter implements IApplicationSpan, IUserSpan, ISpanAdapter {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SpanAdapter.class);
+public class SpanAdapter implements IUserSpan, ISpanAdapter {
     private final Tracer tracer;
     private final TextMapPropagator textMapPropagator;
-
     private final LogRecordBuilder logRecordBuilder;
     private HttpServletRequest request;
+
+    private Span span;
 
     public SpanAdapter(
             final Tracer tracer,
@@ -40,10 +38,6 @@ public class SpanAdapter implements IApplicationSpan, IUserSpan, ISpanAdapter {
         this.textMapPropagator = Objects.requireNonNull(textMapPropagator);
     }
 
-    private String getCorrelationId() {
-        return this.request.getHeader("x-correlation-id");
-    }
-
     private Context makeContext() {
         return textMapPropagator.extract(Context.current(), request, new HeaderGetter());
     }
@@ -51,20 +45,38 @@ public class SpanAdapter implements IApplicationSpan, IUserSpan, ISpanAdapter {
     @Override
     public void startSpan(final String spanName, ICallback function) {
         final var contextResult = this.makeContext();
-        final var span = tracer.spanBuilder(spanName)
-                .setParent(contextResult)
-                .setAttribute("correlationId", getCorrelationId())
+        this.span = tracer.spanBuilder(spanName)
                 .setSpanKind(SpanKind.SERVER)
+                .setParent(contextResult)
                 .startSpan();
-        try(Scope scope = span.makeCurrent()) {
+        final var currrentContext = Context.current().with(this.span);
+        try (Scope scope = this.span.makeCurrent()) {
             try {
-                logRecordBuilder.setContext(contextResult).emit();
+                logRecordBuilder.setContext(currrentContext).emit();
                 function.apply();
             } catch (Exception e) {
-              span.recordException(e);
-              throw e;
+                this.span.recordException(e);
+                throw e;
             } finally {
-                span.end(Instant.now());
+                this.span.end(Instant.now());
+            }
+        }
+    }
+
+    @Override
+    public void startSpanWithContext(String spanName, ICallback function) {
+        final var clientSpan = tracer.spanBuilder(spanName)
+                .setSpanKind(SpanKind.CLIENT)
+                .setParent(Context.current().with(this.span))
+                .startSpan();
+        try (Scope scope = clientSpan.makeCurrent()) {
+            try {
+                function.apply();
+            } catch (Exception e) {
+                clientSpan.recordException(e);
+                throw e;
+            } finally {
+                clientSpan.end(Instant.now());
             }
         }
     }
@@ -72,8 +84,7 @@ public class SpanAdapter implements IApplicationSpan, IUserSpan, ISpanAdapter {
     @Override
     public MessageProperties contextPropagationQueue() {
         final var messageProperties = new MessageProperties();
-        final var context = this.makeContext();
-        messageProperties.setHeader("correlationId", getCorrelationId());
+        final var context = Context.current().with(this.span);
 
         this.textMapPropagator.inject(context, messageProperties.getHeaders(), (carrier, key, value) -> {
             carrier.put(key, value);
@@ -85,7 +96,7 @@ public class SpanAdapter implements IApplicationSpan, IUserSpan, ISpanAdapter {
     @Override
     public List<ClientHttpRequestInterceptor> contextPropagationApi() {
         List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
-        interceptors.add(new HeaderPropagationInterceptor(this.request));
+        interceptors.add(new HeaderPropagationInterceptor());
         return interceptors;
     }
 
