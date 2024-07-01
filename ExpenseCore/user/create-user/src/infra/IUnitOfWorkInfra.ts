8d95835ea83c3cdd9c097e18e4102a6bf86079b8
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-06-30 22:29:44 -0300

    feat: adicionando metodo para buscar usuario

--- ExpenseCore/user/create-user/src/infra/IUnitOfWorkInfra.ts
+++ ExpenseCore/user/create-user/src/infra/IUnitOfWorkInfra.ts
@@ -1,3 +1,6 @@
+import { UserEntity } from "./entities/UserEntity";
+
 export interface IUnitOfWorkInfra {
-  transaction(data: any): Promise<void>;
+  transaction(data: any): Promise<void>
+  findOne(query: { [key: string]: { [key: string]: string } }): Promise<UserEntity | null>
 }
