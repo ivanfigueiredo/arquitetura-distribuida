8df51bc47b5d69e7f4c15022633d890d651ac3e4
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-07-09 13:56:56 -0300

    feat: criando StateManager para salvar Estado entre as transacoes

--- ExpenseCore/core/src/infra/IStateManager.ts
+++ ExpenseCore/core/src/infra/IStateManager.ts
@@ -1,6 +1,6 @@
 export interface IStateManeger {
     set<T>(key: string, data: T, expiresAt?: number): Promise<void>;
-    get<T>(key: string): Promise<T>;
+    get<T>(key: string): Promise<T | null>;
 }
 
 export interface IStateManagerSetup {
