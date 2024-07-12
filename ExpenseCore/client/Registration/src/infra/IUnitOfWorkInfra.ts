62b11b3f0b4ffc381e95f2f6b011ab8e306330f3
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-07-12 16:03:59 -0300

    feat: adicionando metodo delete - hard delete

--- ExpenseCore/client/Registration/src/infra/IUnitOfWorkInfra.ts
+++ ExpenseCore/client/Registration/src/infra/IUnitOfWorkInfra.ts
@@ -4,4 +4,5 @@ import { EntityTarget, ObjectLiteral } from "typeorm";
 export interface IUnitOfWorkInfra {
   transaction<T>(client: T): Promise<void>;
   findOne(clientId: string): Promise<ClientEntity | null>
+  delete<T extends ObjectLiteral>(entityTarget: EntityTarget<T>, criteria: { [key: string]: string }): Promise<void>
 }
