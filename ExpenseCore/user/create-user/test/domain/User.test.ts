77391cc4cffb40fde98e5865dcf16e9d37dffabe
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-07-19 17:31:19 -0300

    test: Deve restaurar um usuário com sucesso

--- ExpenseCore/user/create-user/test/domain/User.test.ts
+++ ExpenseCore/user/create-user/test/domain/User.test.ts
@@ -5,4 +5,8 @@ describe('User', () => {
     test('Deve criar um usuário com sucesso', () => {
         expect(User.create('test@mail.com', 'S&nh@123', 'Individual')).toBeInstanceOf(User)
     })
+
+    test('Deve restaurar um usuário com sucesso', () => {
+        expect(User.restore(randomUUID(), 'test@mail.com', 'S&nh@123', 'Individual')).toBeInstanceOf(User)
+    })
 })
