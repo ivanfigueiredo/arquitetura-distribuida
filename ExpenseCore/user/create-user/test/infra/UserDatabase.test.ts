fbaa3743ab2ea1064af559c1824a89eccc0906e7
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-07-20 11:49:58 -0300

    test: atualizando dependencias dos testes

--- ExpenseCore/user/create-user/test/infra/UserDatabase.test.ts
+++ ExpenseCore/user/create-user/test/infra/UserDatabase.test.ts
@@ -1,27 +1,34 @@
 import { ILogger } from "expense-core"
-import { IUnitOfWorkInfra } from "../../src/infra/IUnitOfWorkInfra"
-import { IUserRepository } from "../../src/application/IUserRepository"
 import { UserDatabase } from "../../src/infra/UserDatabase"
 import { User } from "../../src/domain/User"
 import { InternalServerErrorException } from "../../src/application/exceptions/InternalServerErrorException"
 import { UserEntity } from "../../src/infra/entities/UserEntity"
 import { randomUUID } from "crypto"
+import { DatabaseConnection } from "../../src/infra/DatabaseConnection"
+import { Repository } from "typeorm"
 
 describe('UserDatabase', () => {
-    let unitOfWork: IUnitOfWorkInfra
+    let connection: DatabaseConnection
     let logger: ILogger
-    let userDatabase: IUserRepository
+    let userDatabase: UserDatabase
+    let mockRepository: jest.Mocked<Repository<UserEntity>>;
 
     beforeEach(async () => {
-        unitOfWork = {
-            findOne: jest.fn(),
-            transaction: jest.fn()
+        mockRepository = {
+            save: jest.fn(),
+            findOne: jest.fn()
+        } as any
+        connection = {
+            init: jest.fn(),
+            getDataSourcer: jest.fn().mockReturnValue({
+                getRepository: jest.fn().mockReturnValue(mockRepository)
+            })
         }
         logger = {
             error: jest.fn(),
             info: jest.fn()
         }
-        userDatabase = new UserDatabase(unitOfWork, logger)
+        userDatabase = new UserDatabase(connection, logger)
     })
 
     test('Deve estar definido', () => {
@@ -30,16 +37,16 @@ describe('UserDatabase', () => {
 
     test('Deve salvar um usuário com sucesso', async () => {
         const user = User.create('test@mail.com', 'S&nh@123', 'Individual')
-        const spyOnTransaction = jest.spyOn(unitOfWork, 'transaction')
+        const spyOnSave = jest.spyOn(mockRepository, 'save')
         await userDatabase.save(user)
 
-        expect(spyOnTransaction).toHaveBeenCalledTimes(1)
+        expect(spyOnSave).toHaveBeenCalledTimes(1)
     })
 
     test('Deve lançar uma exceção caso ocorra um erro', async () => {
         const user = User.create('test@mail.com', 'S&nh@123', 'Individual')
         const spyOnLogger = jest.spyOn(logger, 'error')
-        jest.spyOn(unitOfWork, 'transaction').mockImplementation((): never => { throw new Error() })
+        jest.spyOn(mockRepository, 'save').mockImplementation((): never => { throw new Error() })
         const promise = userDatabase.save(user)
         await expect(promise).rejects.toThrow(
             new InternalServerErrorException("Internal server error. If the error persists, contact support", 500)
@@ -48,9 +55,9 @@ describe('UserDatabase', () => {
     })
 
     test('Deve retornar um usuário pelo email', async () => {
-        const spyOnFindOne = jest.spyOn(unitOfWork, 'findOne')
+        const spyOnFindOne = jest.spyOn(mockRepository, 'findOne')
         const user = User.create('test@mail.com', 'S&nh@123', 'Individual')
-        jest.spyOn(unitOfWork, 'findOne').mockResolvedValue(UserEntity.from(user))
+        jest.spyOn(mockRepository, 'findOne').mockResolvedValue(UserEntity.from(user))
         const output = await userDatabase.findUserByEmail('test@mail.com')
         expect(output).toBeInstanceOf(User)
         expect(output).not.toBeNull()
@@ -58,17 +65,17 @@ describe('UserDatabase', () => {
     })
 
     test('Deve retornar nulo caso não seja encontrado nenhum usuário com o e-maail informado', async () => {
-        const spyOnFindOne = jest.spyOn(unitOfWork, 'findOne')
+        const spyOnFindOne = jest.spyOn(mockRepository, 'findOne')
         const output = await userDatabase.findUserByEmail('test@mail.com')
-        expect(output).toBeNull()
+        expect(output).toBeUndefined()
         expect(spyOnFindOne).toHaveBeenCalled()
     })
 
     test('Deve retornar um usuário pelo userId', async () => {
-        const spyOnFindOne = jest.spyOn(unitOfWork, 'findOne')
+        const spyOnFindOne = jest.spyOn(mockRepository, 'findOne')
         const userId = randomUUID()
         const user = User.create('test@mail.com', 'S&nh@123', 'Individual')
-        jest.spyOn(unitOfWork, 'findOne').mockResolvedValue(UserEntity.from(user))
+        jest.spyOn(mockRepository, 'findOne').mockResolvedValue(UserEntity.from(user))
         const output = await userDatabase.findUserByUserId(userId)
         expect(output).toBeInstanceOf(User)
         expect(output).not.toBeNull()
@@ -77,9 +84,9 @@ describe('UserDatabase', () => {
 
     test('Deve retornar nulo caso não seja encontrado nenhum usuário com o userId informado', async () => {
         const userId = randomUUID()
-        const spyOnFindOne = jest.spyOn(unitOfWork, 'findOne')
+        const spyOnFindOne = jest.spyOn(mockRepository, 'findOne')
         const output = await userDatabase.findUserByUserId(userId)
-        expect(output).toBeNull()
+        expect(output).toBeUndefined()
         expect(spyOnFindOne).toHaveBeenCalled()
     })
 })
