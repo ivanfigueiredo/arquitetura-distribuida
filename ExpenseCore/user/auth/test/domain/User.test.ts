a13b232c8bb097f88d964aaf7104e49e7da1593f
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-07-20 16:09:22 -0300

    test: Deve retornar um erro caso o email não esteja verificado

--- ExpenseCore/user/auth/test/domain/User.test.ts
+++ ExpenseCore/user/auth/test/domain/User.test.ts
@@ -1,8 +1,15 @@
 import { randomUUID } from "crypto"
 import { User } from "../../src/domain/User"
+import { DomainException } from "../../src/domain/exception/DomainException"
 
 describe('User', () => {
     test('Deve retornar um usuário com sucesso', () => {
         expect(User.restore(randomUUID(), 'test@mail.com', 'S&nh@123', 'Individual', true)).toBeInstanceOf(User)
+    })
+
+    test('Deve retornar um erro caso o email não esteja verificado', () => {
+        expect(() => User.restore(randomUUID(), 'test@mail.com', 'S&nh@123', 'Individual', false)).toThrow(
+            new DomainException("Email has not been verified.", 422)
+        )
     })
 })
