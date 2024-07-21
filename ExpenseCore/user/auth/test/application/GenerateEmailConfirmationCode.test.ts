80dad8c3a5f21753990f4e076aa32c1877950c16
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-07-20 21:36:39 -0300

    test: Deve salvar o código gerado com sucesso

--- ExpenseCore/user/auth/test/application/GenerateEmailConfirmationCode.test.ts
+++ ExpenseCore/user/auth/test/application/GenerateEmailConfirmationCode.test.ts
@@ -3,6 +3,7 @@ import { IGenerateEmailConfirmationCode } from "../../src/application/IGenerateE
 import { IAuthGateway } from "../../src/application/IAuthGateway"
 import { ICodeRepository } from "../../src/application/ICodeRepository"
 import { GenerateEmailConfirmationCode } from "../../src/application/GenerateEmailConfirmationCode"
+import { Code } from "../../src/domain/Code"
 
 describe('GenerateEmailConfirmationCode', () => {
     let usecase: IGenerateEmailConfirmationCode
@@ -26,5 +27,18 @@ describe('GenerateEmailConfirmationCode', () => {
 
     test('Deve estar definido', () => {
         expect(usecase).toBeDefined()
+    })
+
+    test('Deve salvar o código gerado com sucesso', async () => {
+        const email = 'test@mail.com'
+        const spyOnCreate = jest.spyOn(Code, 'create')
+        const spyOnSave = jest.spyOn(codeRepository, 'save')
+        const spyOnCreateUserNotification = jest.spyOn(authGateway, 'createUserNotification')
+
+        await usecase.execute({ email })
+
+        expect(spyOnCreate).toHaveBeenCalledWith(email)
+        expect(spyOnSave).toHaveBeenCalledTimes(1)
+        expect(spyOnCreateUserNotification).toHaveBeenCalledTimes(1)
     })
 })
