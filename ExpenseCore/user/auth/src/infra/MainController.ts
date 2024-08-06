8ecfe7b1b126b3e5d6dcde50e9e4d16e73f58464
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-08-06 11:07:41 -0300

    feat: resolvendo dependencia

--- ExpenseCore/user/auth/src/infra/MainController.ts
+++ ExpenseCore/user/auth/src/infra/MainController.ts
@@ -1,20 +1,25 @@
-import { IAuth } from '../application/IAuth';
-import { IGenerateEmailConfirmationCode } from '../application/IGenerateEmailConfirmationCode';
+import { IAuth } from '../application/IAuth'
+import { IGenerateEmailConfirmationCode } from '../application/IGenerateEmailConfirmationCode'
+import { IValidationToken } from '../application/IValidationToken';
 import { HttpClient } from "./HttpClient"
 
 export class MainController {
     constructor(
         readonly httpClient: HttpClient,
         readonly auth: IAuth,
-        readonly generateCode: IGenerateEmailConfirmationCode
+        readonly generateCode: IGenerateEmailConfirmationCode,
+        readonly tokenValidate: IValidationToken
 
     ) {
         httpClient.on("post", "/auth", async function (params: any, data: any) {
             const output = await auth.execute(data);
             return output;
-        });
+        })
         httpClient.on("post", "/generate-email-confirmation-code", async function (params: any, data: any) {
             await generateCode.execute(data);
+        })
+        httpClient.on("post", "/validation-token", async function (params: any, data: any) {
+            return tokenValidate.execute(data)
         })
     }
 }
