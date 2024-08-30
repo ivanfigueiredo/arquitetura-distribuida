ed640c5094d37fb225d4ff6f88b17709b1078019
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-08-29 21:33:32 -0300

    feat: resolvendo dependencias

--- ExpenseCore/core/src/main.ts
+++ ExpenseCore/core/src/main.ts
@@ -4,6 +4,8 @@ import { SpanAdapter } from './infra/SpanAdapter';
 import { ISpan } from './infra/ISpan';
 import { ExpenserLogger } from './infra/ExpenseLogger';
 import { StateManager } from './infra/StateManager';
+import { Idempotency } from './infra/idempotency/Idempotency';
+import { RedisAdapter } from './infra/redis';
 
 export class ExpenseCoreMain {
     private rabbitMQAdapter?: RabbitMQAdapter
@@ -19,16 +21,19 @@ export class ExpenseCoreMain {
     public async initialize(serviceInstance: any): Promise<void> {
         const openTelemetrySdk = new OpenTelemetrySDK(this.serviceName, this.command)
         const expenseLogger = new ExpenserLogger(openTelemetrySdk.getLoggerProvider, this.serviceName)
-        const stateManager = new StateManager()
-        await stateManager.connect()
+        const clientRedis = new RedisAdapter()
+        await clientRedis.connection()
+        const stateManager = new StateManager(clientRedis.getRedisClient())
         this.spanAdapter = new SpanAdapter(this.serviceName, '0.0.1')
         this.rabbitMQAdapter = new RabbitMQAdapter(this.spanAdapter, expenseLogger, stateManager)
         await this.rabbitMQAdapter.connect()
+        const idempotency = new Idempotency(clientRedis.getRedisClient())
         serviceInstance.rabbitMQAdapter = this.rabbitMQAdapter
         serviceInstance.span = this.spanAdapter
         serviceInstance.logger = expenseLogger
         serviceInstance.loggerContext = expenseLogger
         serviceInstance.stateManager = stateManager
+        serviceInstance.idempotency = idempotency
         await serviceInstance.init()
     }
 }
