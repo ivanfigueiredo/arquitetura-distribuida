22e0f0583118fe73e7de62d48d1b8128e4feb8b3
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-08-29 21:29:17 -0300

    refactor: refatorando adapter do rabbitMQ

--- ExpenseCore/core/src/infra/IStateManager.ts
+++ ExpenseCore/core/src/infra/IStateManager.ts
@@ -1,9 +1,8 @@
 export interface IStateManeger {
-    set<T>(key: string, data: T, expiresAt?: number): Promise<void>;
-    get<T>(key: string): Promise<T | null>;
+    set<T>(key: string, data: T, expiresAt?: number): Promise<void>
+    get<T>(key: string): Promise<T | null>
 }
 
 export interface IStateManagerSetup {
-    connect(): Promise<void>;
-    setTraceId(traceId: string): void;
+    setTraceId(traceId: string): void
 }
--- ExpenseCore/core/src/infra/StateManager.ts
+++ ExpenseCore/core/src/infra/StateManager.ts
@@ -1,17 +1,11 @@
-import { createClient, RedisClientType } from 'redis';
+import { RedisClientType } from 'redis';
 import { IStateManagerSetup, IStateManeger } from "./IStateManager";
 
 export class StateManager implements IStateManeger, IStateManagerSetup {
-    private client: RedisClientType;
     private traceId: string;
 
-    constructor() {
-        this.client = createClient({ url: 'redis://redis:6379' })
+    constructor(private readonly redisClient: RedisClientType) {
         this.traceId = ''
-    }
-
-    public async connect(): Promise<void> {
-        await this.client.connect()
     }
 
     private getPrefixedKey(key: string): string {
@@ -24,12 +18,12 @@ export class StateManager implements IStateManeger, IStateManagerSetup {
 
     public async set<T>(key: string, data: T, expiresAt?: number): Promise<void> {
         const prefixedKey = this.getPrefixedKey(key)
-        await this.client.set(prefixedKey, JSON.stringify(data), { EX: expiresAt ?? 15000, NX: true })
+        await this.redisClient.set(prefixedKey, JSON.stringify(data), { EX: expiresAt ?? 900.000, NX: true })
     }
 
     public async get<T>(key: string): Promise<T | null> {
         const prefixedKey = this.getPrefixedKey(key)
-        const result = await this.client.get(prefixedKey)
+        const result = await this.redisClient.get(prefixedKey)
         if (!result) return null
         return JSON.parse(result) as T
     }
